4e753e9b1946df4fa33950b3a6f390eba9d631ce
emile.bergin@cpe.fr <emile.bergin@cpe.fr>
2020-10-15 13:52:50 +0200

    Fix bug

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+echo "test";
+error_reporting(-1);
+ini_set('display_errors', 'On');
 session_start();
 
-require('./libs/smarty/Smarty.class.php');
+//require('./libs/smarty/Smarty.class.php');
 
 try {
     $bdd = new PDO("mysql:host=localhost;dbname=webapp", "debian-sys-maint",  "aR7RIRZbiUZw3dYk");
@@ -11,5 +14,5 @@ try {
 	echo "$e";
     die();
 }
-
-require_once('/controllers/home.php');
+require_once('./controllers/home.php');
+?>

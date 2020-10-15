7899d0fc9a4bb9baeddcb81fc245afb87e6e08e4
Corentin Bordes <corentin.bordes@tpi02.cpe.fr>
2020-10-15 16:42:20 +0200

    HTML Header + Fix controllers names

--- SuiviCorentin.md
+++ SuiviCorentin.md
@@ -4,4 +4,8 @@
 
 2ème séance : 
 
-    prévision : Architecture et création des fichiers de la view, listing des fonction necessaires avec la base de donnée. 
+    - Prévision : Architecture et création des fichiers de la view, listing des fonction necessaires avec la base de donnée. 
+
+    - Mise en place de l'architecture après début sur le rôle central du controleur où le fait de donner un rôle a la view. Choix d'adopter le controleur comme élément central.
+
+    -Création des pages php de la vue et des controlleurs associés. Codage de la partie html du header.
--- views/Header.php
+++ views/Header.php
@@ -1,12 +1,32 @@
+
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
+	<meta charset="utf-8">
+	<title>Titre de la page</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="script.js"></script>
 </head>
 <body>
 	<header>
-	<p>Magnifique Header/Bandeau</p>
+		<img href="./models/img/logo"></img>
+		<p>Shity_dropshipping.com</p>
+		<form action="./controlleurs/Connexion.php" method="post">
+  			<input type="text" id="username" name="username" required>
+ 			<input type="password" id="password" name="password" required>
+			<input type="submit" value="Valider">
+		</form> 
+		<nav>
+    		<ol>
+      			<li>
+					<p href="./controlleurs/Home.php">Produits</p>
+				</li>
+				<li>
+					<p href="./controlleurs/Basket">Panier</p>
+				</li>
+				<li>	
+					<p href="./controlleurs/AccountCreation.php">Créer un compte</p>
+			    </li>
+    		</ol>
+  		</nav>
 	</header>

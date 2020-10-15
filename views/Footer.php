b2e9eda2d30d80b94a4fea1ed79821c27916b424
Corentin Bordes <corentin.bordes@tpi02.cpe.fr>
2020-10-15 17:21:41 +0200

    little maj

--- views/Footer.php
+++ views/Footer.php
@@ -1,5 +1,6 @@
 	<footer>
-		<p>Quelques informations de copyright ou bien quelques informations sur l'auteur de l'article.</p>
+		<p>Ceci est un copyright, des poursuites seront effectuer envers toutes les personnes essayant d'extraire des informations ou du style a ce site de dropshipping qualitatif.</p><br>
+		<img src="./models/copyright.jpg" alt="Copyright"></img>
 	</footer> 
 </body>
 </html>

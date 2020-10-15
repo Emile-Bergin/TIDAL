deac2b27ecbf88095ac24c1064895f28ce614724
emile.bergin@cpe.fr <emile.bergin@cpe.fr>
2020-10-15 17:01:49 +0200

    titre + logo

--- controllers/Connexion.php
+++ controllers/Connexion.php
@@ -4,8 +4,8 @@ require_once('models/RessourceController.php');
 
 //Actions
 //DEBUG
-// echo $_POST["username"].$_POST["password"]."<br>"; //Affichage username password
-// echo password_hash($_POST["password"], PASSWORD_DEFAULT)."<br>"; //Affichage password hashé
+echo $_POST["username"].$_POST["password"]."<br>"; //Affichage username password
+echo password_hash($_POST["password"], PASSWORD_DEFAULT)."<br>"; //Affichage password hashé
 
 $_SESSION["connecte"]=$pdo->checkConnexion($_POST["username"],$_POST["password"]);
 if($_SESSION["connecte"]==CONNECT){
@@ -13,5 +13,5 @@ if($_SESSION["connecte"]==CONNECT){
 }
 
 //Vue
-require_once("../controleur/Home.controleur.php");
+//require_once("../controleur/Home.controleur.php");
  ?>
--- views/Header.php
+++ views/Header.php
@@ -3,15 +3,17 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
+	<link rel="icon" href="./models/img/pipe-icon.png" />
 	<title>Titre de la page</title>
-	<link rel="stylesheet" href="style.css">
-	<script src="script.js"></script>
+	<!-- <link rel="stylesheet" href="style.css"> -->
+	<!-- <script src="script.js"></script> -->
 </head>
 <body>
 	<header>
-		<img src="./models/img/logo.jpg" style="width:50px;height:50px;"></img>
-		<p>Shity_dropshipping.com</p>
-		<form action="./controlleurs/Connexion.php" method="post">
+		<img src="./models/img/pipe-icon.png" style="width:50px;height:50px;"></img>
+		<h1>La pipe magique</h1>
+		<p><?=getcwd()?></p>
+		<form action="/controlleurs/Connexion.php" method="post">
   			<input type="text" id="username" name="username" required>
  			<input type="password" id="password" name="password" required>
 			<input type="submit" value="Valider">

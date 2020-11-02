a8a8a56b4fddfb6c87409ec236ac12fbb43bd4cd
Bergin Emile <emile.bergin@cpe.fr>
2020-11-02 20:44:42 +0100

    simplify gest Session

--- views/Header.php
+++ views/Header.php
@@ -18,24 +18,17 @@ var_dump($_SESSION);
 			<img id="logo" src="../models/img/logo.png" alt="Logo pipe" ></img>
 			<h1>La pipe magique</h1>
 			<?php
-			function echoConnexion(){
-				echo '<form id="formConnexion" action="../controllers/Connexion.php" method="post" >
+
+            if ($_SESSION["connecte"] == CONNECT) {
+                echo '<h2><a href="../controllers/Deconnexion.php">Déconnexion</a></h2>';
+            }else{
+                echo '<form id="formConnexion" action="../controllers/Connexion.php" method="post" >
 	  						<input type="text" id="username" name="username" placeholder="identifiant" required>
 				 			<input type="password" id="password" name="password" placeholder="mot de passe" required>
 							<input type="submit" value="Connexion">
 						</form>';
-			}
-			if(isset($_SESSION)) {
-	         	if (isset($_SESSION["connecte"])) {
-		         	if ($_SESSION["connecte"] == CONNECT) {
-						echo '<h2><a href="../controllers/Deconnexion.php">Déconnexion</a></h2>';
-					}else{
-						echoConnexion();					
-					}
-				}else{
-					echoConnexion();				
-				}
-			}			
+            }
+
 			?>
 			
 		</div>
@@ -48,9 +41,13 @@ var_dump($_SESSION);
                 <li>
                     <a class="btnMenuNav" href="../controllers/Basket.php"><h2 class="h2Nav">Panier</h2></a>
                 </li>
-                <li>
-                    <a class="btnMenuNav" href="../controllers/AccountCreation.php"><h2 class="h2Nav">Créer un compte</h2></a>
-                </li>
+                <?php
+                if($_SESSION["connecte"]==DISCONNECT){
+                    echo'<li><a class="btnMenuNav" href="../controllers/AccountCreation.php"><h2 class="h2Nav">Créer un compte</h2></a></li>';
+                }
+                ?>
+
+                ?>
     		</ul>
   		</nav>
 	</header>

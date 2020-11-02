7621c2a9e4c4a4cf65747fea0b7a4ef249af9876
Bergin Emile <emile.bergin@cpe.fr>
2020-11-02 21:19:21 +0100

    Basket + tmp basket

--- controllers/Basket.php
+++ controllers/Basket.php
@@ -7,9 +7,8 @@ if ($_SESSION["connecte"] == CONNECT) { //si il est connecté
     $id=$pdo->getCustomerID($_SESSION["username"]);
     var_dump($id);
     $basket= $pdo->getBasket($id);
-    foreach ($basket as $line){
-        var_dump($line);
-    }
+    var_dump($basket);
+
 }else{
     if(isset($_SESSION["tmpBasket"])){
         echo $_SESSION["tmpBasket"];
